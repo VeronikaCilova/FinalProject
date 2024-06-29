ab59dfab86f29838b02c89635f184af8a2851ca4
Magda <magda.krajewska@seznam.cz>
2024-06-29 14:15:03 +0200

    Model_goal & position

--- viewer/models.py
+++ viewer/models.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import User
 
-from django.db.models import Model, CharField, EmailField, ForeignKey, DO_NOTHING, ImageField, SET_NULL
+from django.db.models import Model, CharField, EmailField, ForeignKey, DO_NOTHING, ImageField, SET_NULL, TextField, \
+    DateField, TextChoices
 
 
-# Create your models here.
+class Position(Model):
+    position_name = CharField(max_length=128)
+    department = CharField(max_length=128)
+
+    class Meta:
+        ordering = ['position_name']
+
+    def __str__(self):
+        return self.position_name
+
+
 class Profile(Model):
     user = ForeignKey(User, on_delete=DO_NOTHING)
     position = ForeignKey(Position, null=True, blank=True, on_delete=DO_NOTHING)
@@ -15,3 +26,29 @@ class Profile(Model):
 
     def __str__(self):
         return f'{self.user.first_name} ({self.user.last_name})'
+
+
+class Priority(TextChoices):
+    HIGH = 'high'
+    MEDIUM = 'medium'
+    LOW = 'low'
+
+
+class Status(TextChoices):
+    TODO = 'todo'
+    DOING = 'doing'
+    DONE = 'done'
+
+
+class Goal(Model):
+    name = CharField(max_length=32)
+    description = TextField(null=True, blank=True)
+    deadline = DateField(null=True, blank=True)
+    priority = CharField(max_length=64, choices=Priority.choices, null=True, blank=True)
+    status = CharField(max_length=10, choices=Status.choices, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name', 'deadline']
+
+    def __str__(self):
+        return f"{self.name} ({self.deadline})"
