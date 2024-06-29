e0456c8030b33a47fc937c04e85c824c85020aa0
Veronika Cilova <veronika.cilova@email.cz>
2024-06-29 14:03:58 +0200

    Task, Feeback, Review Model added

--- viewer/models.py
+++ viewer/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 
 from django.db.models import Model, CharField, EmailField, ForeignKey, DO_NOTHING, ImageField, SET_NULL
+from django.forms import DateTimeField
+from pip._vendor.distlib.markers import Evaluator
 
 
 # Create your models here.
@@ -15,3 +17,30 @@ class Profile(Model):
 
     def __str__(self):
         return f'{self.user.first_name} ({self.user.last_name})'
+
+
+class Review(Model):
+    creation_date = DateTimeField(auto_now_add=True)
+    description = CharField(max_length=500, null=True, blank=True)
+    evaluator = ForeignKey(Profile, on_delete=DO_NOTHING)
+    subject_of_review = ForeignKey(Profile, on_delete=DO_NOTHING)
+    goal = CharField(max_length=500, null=True, blank=True)
+    training = CharField(max_length=250, null=True, blank=True)
+
+
+class Task(Model):
+    creation_date = DateTimeField(auto_now_add=True)
+    note = CharField(max_length=500, null=True, blank=True)
+    to_do_list = CharField(max_length=500, null=True, blank=True)
+    creator = ForeignKey(Profile, on_delete=DO_NOTHING)
+
+
+class Feedback(Model):
+    creation_date = DateTimeField(auto_now_add=True)
+    description = CharField(max_length=500, null=True, blank=True)
+    evaluator = ForeignKey(Profile, on_delete=DO_NOTHING)
+    subject_of_review = ForeignKey(Profile, on_delete=DO_NOTHING)
+
+
+
+
